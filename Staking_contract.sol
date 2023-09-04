1048f4ddf84d5e182590d9030b65fea600d340ce
Esther Breath <estherojile248@gmail.com>
2023-09-04 16:48:09 +0000

    added_reward

--- Staking_contract.sol
+++ Staking_contract.sol
@@ -1,11 +1,10 @@
-
-
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.18;
 import {IStandardToken} from "./IStandardToken.sol";
 
+
 // allow users to stake standardToken
-//able to view the totaltal amount stake by any user
+//able to view the total amount stake by any user
 //allows user to be able withdraw their stake amount
 
 contract StakingContract{
@@ -13,15 +12,20 @@ contract StakingContract{
 struct User{
     uint amountStaked;
     uint timeStaked;
+    uint reward;
 }
+// 0.1 = 10%
+  uint256 public rewardPercentage;
+
 mapping (address => User) user;
 
 event Staked(uint amountstake, uint totalAmountStaked, uint time);
 // allow users to stake standardToken
 
 //set the address of the Token to be staked
-constructor(address _standardToken){
+constructor(address _standardToken, uint256 _rewardPercentage){
     standardToken = IStandardToken(_standardToken);
+    rewardPercentage = _rewardPercentage;
 }
     function stake(uint amount) external {
         uint balance = standardToken.balanceOf(msg.sender);
@@ -31,6 +35,7 @@ constructor(address _standardToken){
         require(status == true, "transfer Failed");
         //update state after confirming transfer of standardToken
         User storage _user = user[msg.sender];
+        _user.reward += calcReward();
         _user.amountStaked += amount;
         _user.timeStaked = block.timestamp;
         emit Staked(amount, _user.amountStaked, block.timestamp);
@@ -53,11 +58,20 @@ constructor(address _standardToken){
     function withdrawEther()  external{
         standardToken.withdrawEther();
         payable(msg.sender).transfer(address(this).balance);
-
-
     }
+
+   function calcReward() public view returns(uint256) {
+     User storage _user = user[msg.sender];
+     uint256 stakeDuration = block.timestamp - _user.timeStaked;
+     return _user.amountStaked * rewardPercentage * stakeDuration;
+   } 
+
+    function getStakedReward() public view {
+      User storage _user = user[msg.sender];
+      uint256 SReward = _user.reward + calcReward();
+      require(SReward > 0, "You did not stake");
+    }
+    
     receive() external payable{}
     fallback() external payable{}
-
-
 }
